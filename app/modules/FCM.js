fd3436e92440ab6469d70498ff1c6834beb91eba
JanErikFoss <janisolafoss96@gmail.com>
2017-02-27 05:59:24 +0100

    Working push system, but push not showing on locked screen

--- app/modules/FCM.js
+++ app/modules/FCM.js
@@ -1,5 +1,5 @@
-import { Platform, Alert } from "react-native"
-import FCM, { FCMEvent, RemoteNotificationResult } from "react-native-fcm"
+import { Alert } from "react-native"
+import FCM, { FCMEvent } from "react-native-fcm"
 
 let db = null
 let uid = null
@@ -16,7 +16,6 @@ module.exports.init = (_db, _uid) => {
 
 FCM.on(FCMEvent.Notification, notif => {
   Alert.alert(notif.notification.title, notif.notification.body)
-  Platform.OS === "ios" && notif.finish(RemoteNotificationResult.NoData)
 })
 
 const refreshListener = FCM.on(FCMEvent.RefreshToken, saveToken)
